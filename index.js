27628a8cf2a6133684f4a405ffd44420f7fb7642
franadam <francoisadam@hotmail.be>
2020-07-22 14:25:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is on port ${port} ! `);
+});
